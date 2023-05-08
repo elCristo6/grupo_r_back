fc3018a88a1298e97afe7656ec2c5c271bc9f48b
elCristo6 <cgonzalezsuta@gmail.com>
2023-05-08 09:25:09 -0500

    Base de datos nueva free

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 const Sequelize = require('sequelize');
 //const sequelize = new Sequelize('mysql://root:91120152349.@localhost/database_r');
-const sequelize = new Sequelize('mysql://root:91120152349.@database-1.ctzhw4x51qx3.us-east-1.rds.amazonaws.com/database_r');
+//const sequelize = new Sequelize('mysql://root:91120152349.@database-1.ctzhw4x51qx3.us-east-1.rds.amazonaws.com/database_r');
+const sequelize = new Sequelize('mysql://root:91120152349.@grupoempresarialrdatabasefree.cihf10rmhl2c.us-east-1.rds.amazonaws.com/database_r');
 module.exports = sequelize;
